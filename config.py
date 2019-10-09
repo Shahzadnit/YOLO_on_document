65c4e18965e74627dedca2825d2edc18064cce82
shahzadnit <shahzadnit@gmail.com>
2019-10-09 16:25:52 +0530

    add files

---
+++ config.py
@@ -0,0 +1,10 @@
+
+VOC_CLASSES = (  # always index 0
+    'comp_name', 'Total_ammount','Invoice_date','Invoice_no','Due_date')
+
+VOC_IMG_MEAN = (123, 117, 104)  # RGB
+
+COLORS = [[255, 0, 0],[0, 255, 0],[0, 0, 255],[127, 0, 127],[0, 250, 255]]
+
+# network expects a square input of this dimension
+YOLO_IMG_DIM = 448
